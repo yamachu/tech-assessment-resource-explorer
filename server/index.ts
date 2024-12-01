175ccde3da7ee1c3d10cc053bf5597132245b8ff
yamachu <yamachu.dev@gmail.com>
2024-12-01 15:16:32 +0900

    add tree, content, image API

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,11 @@
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
 import { Hono } from "hono";
 import { Octokit } from "octokit";
+import {
+  fetchRepositoryBinaryContent,
+  fetchRepositoryContent,
+  fetchRepositoryTree,
+} from "./utils/github";
 
 type Env = {
   Variables: {
@@ -44,9 +49,41 @@ app.use(async (c, next) => {
   await next();
 });
 
-app.get("/api", (c) => {
+app.get("/api/tree/:owner/:repo", async (c) => {
+  const { owner, repo } = c.req.param();
+  const tree = await fetchRepositoryTree(c.get("octokit"), {
+    owner,
+    repo,
+  });
+
   return c.json({
-    message: "Hello",
+    tree,
+  });
+});
+
+app.get("/api/content/:owner/:repo/:path{.*}", async (c) => {
+  const { owner, repo, path } = c.req.param();
+  const body = await fetchRepositoryContent(c.get("octokit"), {
+    owner,
+    repo,
+    path,
+  });
+
+  return c.json({
+    body,
+  });
+});
+
+app.get("/api/image/:owner/:repo/:path{.*}", async (c) => {
+  const { owner, repo, path } = c.req.param();
+  const buffer = await fetchRepositoryBinaryContent(c.get("octokit"), {
+    owner,
+    repo,
+    path,
+  });
+
+  return c.body(buffer.buffer, 200, {
+    "Content-Type": "application/octet-stream",
   });
 });
 
